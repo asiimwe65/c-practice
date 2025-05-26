5155d032c0bb2d7ab5d93928d1cbc9d2e2aff1ad
ASIIMWE ISAAC <asiimweisaac40@gmail.com>
2025-05-27 01:23:36 +1000

    working on a single basic user input in c and code-runner.runInTerminal: true was placed in the settings json of vscode to enable user inputs

--- app.c
+++ app.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 int main(){
-    int age= 25;
-    printf("Age :%d", age);
-    age=36;
-    printf("\nNew age: %d",age);
-    double number = 12.45;
-    printf("\n decimal number : %.2lf",number);
-    float number1=9.9f;
-    printf("\n float number: %.1f",number1);
-    char character='z';
-    printf("\nstring letter:  %c", character);
-    printf("\n int size: %zu",sizeof(age));
-    printf("\n double size: %zu",sizeof(age));
+    double year;
+
+    printf("Enter your age: ");
+
+    scanf("%lf", &year);
+
+    printf("Your age is %2lf",year);
+
     return 0;
 }
