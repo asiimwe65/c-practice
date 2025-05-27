37870640f18a7c7fdd0a4b2eebdd4e84fa0ebc7b
ASIIMWE ISAAC <asiimweisaac40@gmail.com>
2025-05-27 23:06:32 +1000

    I practiced operators in c programming language

--- app.c
+++ app.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main(){
     double year;
+    char alphabet;
 
     printf("Enter your age: ");
 
     scanf("%lf", &year);
 
-    printf("Your age is %2lf",year);
+    printf("Your age is %2lf\n",year);
+
+    printf("\nEnter an alphabet letter: ");
+
+    scanf(" %c",&alphabet);
+
+    printf("The alphabet you have chosen is %c",alphabet);
 
     return 0;
 }
---
+++ operators.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int x=8;
+    int result = x+7;
+    printf("the result %d",result);
+    
+
+     double p=2.13;
+     printf("\nthe second result is %.2lf",p+7);//addition
+     //division we use the ssign / but we use the double datatype tbecause we can get decimals
+     double z=13;
+     printf("\nThe third result is %.2lf",z/2);
+     
+     // finding the modular operator you can use the inter datatype
+    int j=9;
+    printf("\n the remainder to 9 divided by 7 is %d",j%7);
+    // increment and decrement operators
+    int h=5;
+    printf("\n the 4th result is %d",++h);
+    printf("\n the 5th result is %d",--h);
+    // multiple operators in one line
+    double n=12/6*3-1;
+    printf("\n the 6th result is %.0lf",n);
+    return 0;
+}
