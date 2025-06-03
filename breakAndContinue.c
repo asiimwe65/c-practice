8e0423b044bc30e1d0135f0b2447e77311aded67
ASIIMWE ISAAC <asiimweisaac40@gmail.com>
2025-06-03 23:05:23 +1000

    break and continue

--- breakAndContinue.c
+++ breakAndContinue.c
@@ -17,18 +17,16 @@
 // continue iteration skips that condition and continues with the rest until it reaches satiety
 #include <stdio.h>
 
-int main() {
-    int number = 0;
-    for (int i = 0; i < 10; i++) {
-        number = number + i;
-
-        if (number == 2) {
-            continue; // Skip printing this step if number is 2
+int main(){
+    int number =0;
+    for(int i= 0;i<11;i++){
+        number=number+1;
+        if(number==2){
+            continue;
         }
+        printf("after adding %d,number=%d\n",i,number);
 
-        printf("After adding %d, number = %d\n", i, number);
     }
-
-    printf("Final value of number: %d\n", number);
+    printf("the final answer is %d\n",number);
     return 0;
 }
